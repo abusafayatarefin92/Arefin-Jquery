d474dc9e44cd0615e86824061a88d834938e4546
abusafayatarefin92 <abusafayatarefin92@gmail.com>
2022-03-31 14:31:04 +0600

    commit

---
+++ tutorial/index.js
@@ -0,0 +1,179 @@
+$(document).ready(function(){
+    // $('#readyDemo').text("The Webpage is ready!!!").css('color', 'red').fadeIn('slow')
+    // $("item-list").text()
+    // $('button').click(function(){
+    //     //$("#item-list").find('a').css('color', 'green')
+    //     // $("#first-li").children('a').css('color', 'red')
+    //     // let answer = $("#answer-box").val()
+
+    //     // if (answer == 'Dhaka') {
+    //     //     $("#result").text("Your answer is correct")
+    //     // } else {
+    //     //     $("#result").html("<span style='color:red'>Your answer is incorrect!</span>")
+    //     //     $("#answer").text("The correct answer is Dhaka")
+    //     // }
+    // })
+
+    // $("p").dblclick(function(){
+    //     $(this).hide()
+    // })
+
+    // $("p").mouseenter(function(){
+    //         alert ("You entered into p")
+    //     })
+    
+    // $("p").mouseleave(function(){
+    //         alert ("You have just left")
+    //     })
+
+    // $("p").mousedown(function(){
+    //     alert ("Mouse down over")
+    // })
+
+    // $("p").mouseup(function(){
+    //     alert ("Mouse down over")
+    // })
+
+    // $("p").hover(function(){
+    //     alert ("Mouse down over")
+    // },
+    // function(){
+    //     alert("Bye! now you leave")
+    // }
+    // )
+
+    // $("input").focus(function(){
+    //     $(this).css("background-color", "yellow")
+    // })
+
+    // $("input").blur(function(){
+    //     $(this).css("background-color", "green")
+    // })
+
+    // $("p").on({
+    //     mouseenter: function(){
+    //         $(this).css("background-color", "lightgrey")
+    //     },
+
+    //     mouseleave: function(){
+    //         $(this).css("background-color", "lightblue")
+    //     },
+
+    //     click: function(){
+    //         $(this).css("background-color", "yellow")
+    //     }
+    // })
+
+    // $("#hide").click(function(){
+    //     $("p").hide(1000)
+    // })
+    // $("#show").click(function(){
+    //     $("p").show(1000)
+    // })  
+    
+    // $("button").click(function(){
+    //     $("p").toggle()
+    // })
+
+    // $("button").click(function(){
+    //     $("#div1").fadeIn()
+    //     $("#div2").fadeIn("slow")
+    //     $("#div3").fadeIn(3000)
+    // })
+
+    // $("button").click(function(){
+    //     $("#div1").fadeOut()
+    //     $("#div2").fadeOut("slow")
+    //     $("#div3").fadeOut(3000)
+    // })
+
+    // $("button").click(function(){
+    //     $("#div1").fadeToggle()
+    //     $("#div2").fadeToggle("slow")
+    //     $("#div3").fadeToggle(3000)
+    // })
+
+    // $("button").click(function(){
+    //     $("#div1").fadeTo("slow", 0.15)
+    //     $("#div2").fadeTo("slow", 0.3)
+    //     $("#div3").fadeTo("slow", 0.5)
+    // })
+
+    // $("#btn1").click(function(){
+    //     alert("Text: " + $("#test").text())
+    // })
+
+    // $("#btn2").click(function(){
+    //     alert("HTML: " + $("#test").html())
+    // })
+
+    // $("button").click(function(){
+    //     alert("Value: " + $("#test").val())
+    // })
+
+    // $("button").click(function(){
+    //     alert($("#w3s").attr("href"))
+    // })
+
+    // $("#btn1").click(function(){
+    //     $("#test1").text("Hello world!")
+    // })
+
+    // $("#btn2").click(function(){
+    //     $("#test2").html("<b>Hello world!</b>")
+    // })
+
+    // $("#btn3").click(function(){
+    //     $("#test3").val("Mouse")
+    // })
+
+    // $("#btn1").click(function(){
+    //     $("#test1").text(function(i, origText){
+    //         return "Old text: " + origText + " New text: Hello World! index(" + i + ")"
+    //     })
+    // })
+
+    // $("#btn2").click(function(){
+    //     $("#test2").text(function(i, origText){
+    //         return "Old html: " + origText + " New html: Hello World! index(" + i + ")"
+    //     })
+    // })
+
+    // $("button").click(function(){
+    //     $("#w3s").attr("href", "https://www.w3schools.com/jQuery")
+    // })
+
+    // $("#btn1").click(function(){
+    //     $("p").append("<b>Append</b>")
+    // })
+
+    // $("#btn2").click(function(){
+    //     $("ol").append("<li>Append</li>")
+    // })
+
+    // $("#btn1").click(function(){
+    //     $("p").before("<b>Before</b>")
+    // })
+
+    // $("#btn2").click(function(){
+    //     $("ol").after("<li>After</li>")
+    // })
+
+    // $("button").click(function(){
+    //     $("#div1").remove()
+    // })
+
+    // $("button").click(function(){
+    //     $("#div1").empty()
+    // })
+
+    // $("button").click(function(){
+    //     $("p").remove(".test, .demo")
+    // })
+
+    // $("button").click(function(){
+    //     $("h1, h2, p").addClass("blue")
+    //     $("div").addClass("important")
+    // })
+
+})
